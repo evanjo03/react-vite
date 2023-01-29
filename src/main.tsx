3587cfba6fdffff3020973924b7a41c4f1e4fab9
John Evans <john.wes.evans@gmail.com>
2023-01-28 23:35:37 -0600

    Update to use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ProSidebarProvider } from "react-pro-sidebar";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
